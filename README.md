781eb17b63b4f1e6aa80d3fc9264d5f34bacffe3
Michael Stevens <okyfunoky@hotmail.com>
2019-09-24 20:15:09 -0700

    Starting on readme and adding some protections around spotify

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # liri-node-app
+Welcome to LIRI, a node based helper.
+LIRI uses NPM packages to get you relevant information  on your queries.
+
+# Node Instructions
+Install Node.js on your system:
+https://nodejs.org/en/
+
+# NPM Instructions
+After you've installed node, you should have NPM installed. In the root directory of this repository, run "npm install" to download the required packages for this application. 
+For reference, these are the required packages at the time of this writing:
+    "axios": "^0.19.0",
+    "dotenv": "^8.1.0",
+    "moment": "^2.24.0",
+    "node-spotify-api": "^1.1.1"
+
+
+# dotenv Instructions
+In order to use LIRI's spotify functionality, you'll need to register for a spotify developer account.
+Once you have a clientId and Key, you'll need to make a file called .env, and enter data in this format exactly:
+
+This will configure the application to use your spotify credentials in a secure way.
+
+# Commands
+If you're unfamiliar with how to run node applications, navigate the to the root directory of this repositiory in a command prompt window. Then run an example command (details below).
+
+LIRI accepts four commands:
+
+1) Spotify-this-song "title of song"
+    ex: "node .\liri.js spotify-this Knights of Cydonia"
+2) movie-this "movie title"
+    ex: "node .\liri.js movie-this Jurassic Park"
+3) concert-this "band name"
+    ex: "node .\liri.js concert-this Muse"
+4) do-what-it-says 
+
+1) Searches for the top spotify song of the song name specified and returns some details, including a preview link
+
+2) Searches OMDB and returns releveant details for the movie selected
+
+3) Searches the "BandsInTown" api and returns the next concert for the specified band.
+
+4) ????
+
--- liri.js
+++ liri.js
@@ -1,14 +1,22 @@
-let axios = require('axios');
-let dotenv = require('dotenv').config();
-let moment = require('moment');
-let Spotify = require('node-spotify-api');
-let keys = require('./keys.js');
-let fs = require('fs');
+const axios = require('axios');
+const dotenv = require('dotenv').config();
+const moment = require('moment');
+const Spotify = require('node-spotify-api');
+const keys = require('./keys.js');
+const fs = require('fs');
 
-var spotify = new Spotify({
-    id: keys.spotify.id,
-    secret: keys.spotify.secret
-});
+const envPath = './.env'
+let spotifyUsable = false;
+try {
+    if (fs.existsSync(envPath)) {
+        var spotify = new Spotify({
+            id: keys.spotify.id,
+            secret: keys.spotify.secret
+        });
+    }
+} catch(err){
+    writeLog("Env file unavailable, spotify is unusable")
+}
 
 requestType = process.argv[2];
 args = process.argv.slice(3).join(" ");
@@ -18,7 +26,11 @@ switch (requestType) {
         concert(args);
         break;
     case "spotify-this-song":
-        spotifySearch(args);
+        if(spotifyUsable){
+            spotifySearch(args);
+        }else{
+            writeLog("Configure your spotify key to use this feature of the app. See README.md for details")
+        }
         break;
     case "movie-this":
         movie(args);
@@ -32,78 +44,88 @@ switch (requestType) {
 
 function concert(bandName) {
     let queryUrl = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp"
+    writeLog("Command: Cocert-This");
+    writeLog("Input:" + bandName);
     axios.get(queryUrl).then(
-        function(response){
-            writeLog(response);
+        function (response) {
             writeLog("Venue Name:" + response.data[0].venue.city);
             writeLog("Venue Location:" + response.data[0].venue.name);
-            writeLog("Date: " + response.data[0].datetime);
+            moment().format();
+            let date = moment(response.data[0].datetime);
+            writeLog("Date: " + String(date));
         }
-    ).catch(function(err){
+    ).catch(function (err) {
         throw err;
     })
 }
 
 function spotifySearch(song) {
-    if(!song){
+    writeLog("Command: Spotify-This");
+    writeLog("Input:" + song);
+    if (!song) {
         song = "All the Small Things"
     }
     spotify
-    .search({ type: 'track', query: song })
-    .then(function(response) {
-      //console.log(response.tracks.items[0]);
-      console.log("Album: " + response.tracks.items[0].album.name);
-      console.log("Artist: " + response.tracks.items[0].artists[0].name);
-      console.log("Link: " + response.tracks.items[0].preview_url);
-      console.log("Name: " + response.tracks.items[0].name);
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
+        .search({ type: 'track', query: song })
+        .then(function (response) {
+            //console.log(response.tracks.items[0]);
+            writeLog("Album: " + response.tracks.items[0].album.name);
+            writeLog("Artist: " + response.tracks.items[0].artists[0].name);
+            writeLog("Link: " + response.tracks.items[0].preview_url);
+            writeLog("Name: " + response.tracks.items[0].name);
+        })
+        .catch(function (err) {
+            writeLog(err);
+        });
 }
 
 function movie(movieName) {
-    if(!movieName){
+    writeLog("Command: movie-This");
+    writeLog("Input:" + movieName);
+    if (!movieName) {
         movieName = "Mr. Nobody"
     }
     let queryUrl = `http://www.omdbapi.com/?t=${movieName}&y=&plot=short&apikey=trilogy`
     axios.get(queryUrl).then(
-        function(response){
+        function (response) {
             writeLog("Title: " + response.data.Title);
             writeLog("Year: " + response.data.Year);
             writeLog("Rating:" + response.data.Rated);
-            writeLog("Country:"+ response.data.Country);
+            writeLog("Country:" + response.data.Country);
             writeLog("Languages: " + response.data.Language);
             writeLog("Plot: " + response.data.Plot);
             writeLog("Cast:" + response.data.Actors);
             writeLog("Rotten Tomatoes: " + response.data.Ratings[1].Value);
         }
-    ).catch(function(err){
+    ).catch(function (err) {
         throw err;
     })
 }
 
 function doWhatItSays() {
-    fs.readFile("random.txt", "utf8", function(error, data) {
+    writeLog("Command: Do-What-It-Says");
+    writeLog("Input:" + "bandName");
+    fs.readFile("random.txt", "utf8", function (error, data) {
 
         if (error) {
-          return writeLog(error);
+            return writeLog(error);
         }
 
         writeLog(data);
-        console.log(data);
-      
-      });
-      
+
+    });
+
 }
 
-function writeLog(input){
-    logValue = input
+function writeLog(input) {
+    logValue = input + "\n"
     console.log(logValue);
-    fs.appendFile("log.txt", logValue, function(err) {
-        
+
+    fs.appendFile("log.txt", logValue, function (err) {
+
         if (err) {
-          console.log(err);
+            console.log("Error writing log: " + err);
         }
-      });
+    });
+
 }
